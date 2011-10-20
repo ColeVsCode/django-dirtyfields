40996f797afe20afc12c91b4d9493e3a59403d29
Cole <cole@koduco.com>
2011-10-19 18:04:38 -0700

    Including foreignkey/onetoone fields in dirty field list as implemented here: http://stackoverflow.com/questions/110803/dirty-fields-in-django/4676107#4676107

--- src/dirtyfields/dirtyfields.py
+++ src/dirtyfields/dirtyfields.py
@@ -9,8 +9,8 @@ class DirtyFieldsMixin(object):
         reset_state(sender=self.__class__, instance=self)
 
     def _as_dict(self):
-        return dict([(f.name, getattr(self, f.name)) for f in self._meta.local_fields if not f.rel])
-    
+        return dict([(f.attname, getattr(self, f.attname)) for f in self._meta.local_fields])
+            
     def get_dirty_fields(self):
         new_state = self._as_dict()
         return dict([(key, value) for key, value in self._original_state.iteritems() if value != new_state[key]])
